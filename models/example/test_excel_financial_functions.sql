35bcdade43ffb608a8024e3c893190fbe3992bd1
Tony Johnson <subscribe@alj.id.au>
2025-06-12 17:23:26 +0000

    Get seed and tests compiling. Fix recursion in functions.

---
+++ models/example/test_excel_financial_functions.sql
@@ -0,0 +1,21 @@
+with
+    params as (
+        select rate, nper, pv, start_period, end_period, type
+        from {{ ref("seed_test_excel_functions") }}
+    ),
+    calculated as (
+        select
+            rate,
+            nper,
+            pv,
+            type,
+            {{ pmt("rate", "nper", "pv", "type") }} as pmt,
+            {{ cumipmt("rate", "nper", "pv", "start_period", "end_period", "type") }}
+            as cumipmt,
+            {{ cumprinc("rate", "nper", "pv", "start_period", "end_period", "type") }}
+            as cumprinc
+        from params
+    )
+
+select rate, nper, pv, type, pmt, cumipmt, cumprinc
+from calculated
